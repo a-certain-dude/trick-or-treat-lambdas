485db501412cb8cea6bf7f7bbe8e390c0fd34505
midnight_bundle <nkwantabisajoseph25@gmail.com>
2025-12-27 10:30:37 +0000

    convert function to lambda expression

--- src/Main.kt
+++ src/Main.kt
@@ -3,11 +3,15 @@ fun main() {
     treat()
 }
 
-
-
+/*
+fun trick () {
+    println("no treat")
+    // below converts it to a variable
+}*/
 val trick = {
     println("No Treats")
 }
+
 val treat = {
     println("Have a treat")
 }
